f99c9b86aaccd288675e32aac57d3011f1110b10
Jaskirat Singh <74609729+2912001@users.noreply.github.com>
2022-07-26 14:01:23 +0530

    Create Assignment.java

---
+++ Assignment.java
@@ -0,0 +1,93 @@
+                                  // Java Assignment-1
+								  
+								  
+								  
+	/*Q5
+	
+	
+	 Wrapper classes basically converts the primitive data type into an object ,
+	 because java language deals with objects and classes .
+      For example:- 
+	               class Wrapper{
+					   public static void main(String [] args){
+						   int a ;
+						   Integer i2 =20;
+						   System.out.println(i2);}
+				   }
+	        ********************************************************
+	Q3
+
+	 1: .java file stores the code of the program whereas .class file stores 
+	 address of that code.
+	 
+	 
+	 2:  Java basically based on the classes and objects which makes it an  
+	 OOP's Programming language but the primitive data types are not objects so 
+	 to solve it wrapper classes used to make these an object and then java will 
+	 be complete OOP's Programming language.
+	
+     3: NR-WA
+	        Example :-  void addition (int a, int b){
+				int c;
+				c = a+b; 
+				 System.out.println(c);
+			}
+               **********************************************************
+ 
+    Q2
+	
+        Naming convention:
+			 Example:-  
+			           Class name = class Method{ Sample code }
+					                class TextComponent{ Sample code }
+					   
+					   Function name = void main (){ Sample code }
+					                   int  add (){ Sample code }
+					   
+					   Variable = 
+					   Static variable =
+					   
+			**************************************************************		   
+	*/
+	//Q4
+	class Calci{
+	public static void main(String [] args){
+		Calci obj= new  Calci();
+		obj.mull();
+	}
+	
+	static void addy(){
+	}
+	static void subby(){
+	}
+	 void diviji(){
+	}
+	
+	void mull(){
+		System.out.println("Hello");
+}
+}
+    class CalciWorking{
+		public static void main(String [] args){
+			
+		}
+		
+	}
+	//Q1
+	
+	class Jassi{
+	public static void main(String [] args){
+	String name ="Jaskirat singh";
+	int age = 20;
+	System.out.println(name);
+	System.out.println(age);
+	
+	
+	Jassi obj =new Jassi();
+	obj.hobby();}
+static void hobby(){
+	System.out.println("Hello");
+}
+	
+	
+}	
